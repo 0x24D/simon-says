1e3ed72a3e301bda09a1186759f93b540f9b8964
0x24d <daniel.longfield@tuta.io>
2019-05-31 15:59:12 +0100

    Implement basic logic of game, need to overwrite shown chars

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,54 @@
+#include <chrono>
+#include <deque>
 #include <iostream>
+#include <random>
+#include <thread>
 
-int main() {
-    std::cout << "Hello World\n";
-    return 0;
+#define BLUE 0
+#define GREEN 1
+#define RED 2
+#define YELLOW 3
+
+char getColour(const int &num) {
+  switch (num) {
+  case BLUE:
+    return 'B';
+  case GREEN:
+    return 'G';
+  case RED:
+    return 'R';
+  case YELLOW:
+    return 'Y';
+  }
 }
 
+int main() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, 3);
+  bool endGame = false;
+  std::deque<char> colours;
+  do {
+    colours.emplace_back(getColour(dis(gen)));
+    std::cout << "Colour sequence:";
+    for (char c : colours) {
+      std::cout << c;
+      // need to sleep and print carriage return to overwrite char
+    }
+    std:: cout << "\nEnter colour sequence: ";
+    for (char c : colours) {
+      char user;
+      std::cin >> user;
+      if (toupper(user) != c) {
+        endGame = true;
+        break;
+      }
+    }
+    if (endGame) {
+      std::cout << "Incorrect sequence! Ending game.\n";
+    } else {
+      std::cout << "Correct sequence! Next level starting.\n";
+    }
+  } while (!endGame);
+  return 0;
+}
